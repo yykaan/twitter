ef47a9c97275b5b4e112b1c5146aadfaeee4734c
Kaan Yılmaz <yyilmazkaan@gmail.com>
2022-04-17 20:35:56 +0300

    Year-month-day update
    Her yılın,  her ayın başlangıç-sonraki ay başlangıç dönemine göre tweet arama

--- twitter/src/main/java/org/example/App.java
+++ twitter/src/main/java/org/example/App.java
@@ -36,57 +36,62 @@ public class App
 
 
         for (String year : years) {
-            driver.get("https://twitter.com/search-advanced?lang=tr");
-            driver.manage().window().maximize();
-            driver.findElement(By.xpath("//*[@id=\"layers\"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div/div[2]/div/div[5]/div[1]/div/label/div/div[2]/div/input"))
-                    .sendKeys("@bloombergHT");
+            try {
+                driver.get("https://twitter.com/search-advanced?lang=tr");
+                driver.manage().window().maximize();
+                driver.findElement(By.xpath("//*[@id=\"layers\"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div/div[2]/div/div[5]/div[1]/div/label/div/div[2]/div/input"))
+                        .sendKeys("@bloombergHT");
 
-            Select selectFromYear = new Select(driver.findElement(By.xpath("/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div/div[2]/div/div[16]/div/div[2]/div/div[3]/select")));
-            Select selectToYear = new Select(driver.findElement(By.xpath("/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div/div[2]/div/div[16]/div/div[4]/div/div[3]/select")));
-            selectFromYear.selectByValue(year);
-            selectToYear.selectByValue(year);
-            for (String month : months) {
-                Select selectFromMonth = new Select(driver.findElement(By.xpath("/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div/div[2]/div/div[16]/div/div[2]/div/div[1]/select")));
-                Select selectToMonth = new Select(driver.findElement(By.xpath("/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div/div[2]/div/div[16]/div/div[4]/div/div[1]/select")));
-                selectFromMonth.selectByValue(month);
-                selectToMonth.selectByValue(String.valueOf(Integer.parseInt(month)+1));
+                Select selectFromYear = new Select(driver.findElement(By.xpath("/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div/div[2]/div/div[16]/div/div[2]/div/div[3]/select")));
+                Select selectToYear = new Select(driver.findElement(By.xpath("/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div/div[2]/div/div[16]/div/div[4]/div/div[3]/select")));
+                selectFromYear.selectByValue(year);
+                selectToYear.selectByValue(year);
+                for (String month : months) {
+                    Select selectFromMonth = new Select(driver.findElement(By.xpath("/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div/div[2]/div/div[16]/div/div[2]/div/div[1]/select")));
+                    Select selectToMonth = new Select(driver.findElement(By.xpath("/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div/div[2]/div/div[16]/div/div[4]/div/div[1]/select")));
+                    selectFromMonth.selectByValue(month);
+                    selectToMonth.selectByValue(String.valueOf(Integer.parseInt(month)+1));
 
-                Select selectFromDay = new Select(driver.findElement(By.xpath("/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div/div[2]/div/div[16]/div/div[2]/div/div[2]/select")));
-                Select selectToDay = new Select(driver.findElement(By.xpath("/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div/div[2]/div/div[16]/div/div[4]/div/div[2]/select")));
-                selectFromDay.selectByValue("1");
-                selectToDay.selectByValue("1");
+                    Select selectFromDay = new Select(driver.findElement(By.xpath("/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div/div[2]/div/div[16]/div/div[2]/div/div[2]/select")));
+                    Select selectToDay = new Select(driver.findElement(By.xpath("/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div/div[2]/div/div[16]/div/div[4]/div/div[2]/select")));
+                    selectFromDay.selectByValue("1");
+                    selectToDay.selectByValue("1");
 
-                driver.findElement(By.xpath("//*[@id=\"layers\"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div/div[1]/div/div/div/div/div/div[3]/div")).click();
+                    driver.findElement(By.xpath("//*[@id=\"layers\"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div/div[1]/div/div/div/div/div/div[3]/div")).click();
 
-                JavascriptExecutor js = (JavascriptExecutor) driver;
-                for (int i = 1; i < 50000000; i++) {
-                    try {
-                        Thread.sleep(2500);
-                        List<WebElement> article = driver.findElements(By.tagName("article"));
-                        for (WebElement webElement : article) {
-                            String tweet = webElement.getText();
-                            String[] tweetDecomposed = tweet.split("\n");
-                            String displayName = tweetDecomposed[0];
-                            String username = tweetDecomposed[1];
-                            String date = tweetDecomposed[3];
-                            StringBuilder tweetBuilder = new StringBuilder();
-                            if (tweetDecomposed.length > 8) {
-                                for (int k = 4; k + 3 <= tweetDecomposed.length - 4; k++) {
-                                    tweetBuilder.append(tweetDecomposed[k]).append("\n");
+                    JavascriptExecutor js = (JavascriptExecutor) driver;
+                    for (int i = 1; i < 2; i++) {
+                        System.out.println(i);
+                        try {
+                            Thread.sleep(2500);
+                            List<WebElement> article = driver.findElements(By.tagName("article"));
+                            for (WebElement webElement : article) {
+                                String tweet = webElement.getText();
+                                String[] tweetDecomposed = tweet.split("\n");
+                                String displayName = tweetDecomposed[0];
+                                String username = tweetDecomposed[1];
+                                String date = tweetDecomposed[3];
+                                StringBuilder tweetBuilder = new StringBuilder();
+                                if (tweetDecomposed.length > 8) {
+                                    for (int k = 4; k + 3 <= tweetDecomposed.length - 4; k++) {
+                                        tweetBuilder.append(tweetDecomposed[k]).append("\n");
+                                    }
+                                }else {
+                                    tweetBuilder.append(tweetDecomposed[4]);
                                 }
-                            }else {
-                                tweetBuilder.append(tweetDecomposed[4]);
-                            }
 
-                            String tweetText = tweetBuilder.toString();
-                            Tweet tweetObject = new Tweet(displayName, username, date, tweetText);
-                            tweets.add(tweetObject);
+                                String tweetText = tweetBuilder.toString();
+                                Tweet tweetObject = new Tweet(displayName, username, date, tweetText);
+                                tweets.add(tweetObject);
+                            }
+                            js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
+                        } catch (Exception e) {
+                            System.out.println(e.getMessage());
                         }
-                        js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
-                    } catch (Exception e) {
-                        System.out.println(e.getMessage());
                     }
                 }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
             }
         }
 
